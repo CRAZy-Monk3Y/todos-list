9f08a8127a162c1d4e842c917ac94b9ffd1d16d1
Tathagata Chakraborty <61090388+CRAZy-Monk3Y@users.noreply.github.com>
2022-07-13 14:17:37 +0530

    update with todos

---
+++ src/MyComponents/Todos.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Todo } from './Todo'
+
+
+export const Todos = (props) => {
+  return (
+    <div className='container'>
+        <h3>Todos List</h3>
+        
+        <Todo></Todo>
+    </div>
+  )
+}
