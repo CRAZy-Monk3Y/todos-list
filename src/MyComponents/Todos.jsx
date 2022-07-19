1b8ab8e04dfb0876f3f87576bb39d52982816803
Tathagata Chakraborty <61090388+CRAZy-Monk3Y@users.noreply.github.com>
2022-07-19 21:07:05 +0530

    final commit

--- src/MyComponents/Todos.jsx
+++ src/MyComponents/Todos.jsx
@@ -8,11 +8,7 @@ export const Todos = (props) => {
       <h3>Todos List</h3>
       {props.todos.length === 0 ? "No todos to display" :
         props.todos.map((todo) => {
-          return (
-            <>
-              <Todo todo={todo} key={todo.sno} onDelete={props.onDelete} /> <hr/>
-            </>
-          )
+          return (<Todo todo={todo} key={todo.sno} onDelete={props.onDelete} />)
         })
       }
     </div>
