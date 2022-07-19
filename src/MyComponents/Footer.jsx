9cd7fb61ae160adddc537e0d848476b6663c466e
Tathagata Chakraborty <61090388+CRAZy-Monk3Y@users.noreply.github.com>
2022-07-19 18:09:12 +0530

    added footer component with style

--- src/MyComponents/Footer.jsx
+++ src/MyComponents/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 export const Footer = () => {
+    let footerStyle = {
+        opacity: '70%', 
+        position: 'absolute', 
+        top: '90vh', 
+        width: '100%'
+    }
     return (
-        <div>
-            Footer Works
-
+        <div className='bg-dark text-light py-3' style={footerStyle}>
+            <p>
+                Copyright &copy; MyTodoList.com
+            </p>
         </div>
     )
 }
